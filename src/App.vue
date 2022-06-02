7010104eb44981c0086ed6c99567aefcfe5bc958
fsociety92 <emil.1emil92@gmail.com>
2022-06-02 07:23:20 +0200

    main

--- src/App.vue
+++ src/App.vue
@@ -2,33 +2,18 @@
   <div>
     <RouterLink to="/">Home</RouterLink> |
     <RouterLink to="/products">Products</RouterLink>
-    <RouterLink :to="{name: 'home'}">Home</RouterLink> |
-    <RouterLink :to="{name: 'products'}">About</RouterLink>
     <RouterView />
   </div>
 </template>
 
-<script lang="ts">
+<script>
 export default {
-  name: "App",
-};
+  setup() {
+
+  }
+}
 </script>
 
+<style>
 
-
-<style scoped>
-@import url('https://fonts.googleapis.com/css2?family=DM+Sans&display=swap');
-
-div {
-  font-family: 'DM Sans', sans-serif;
-  font-size: 14px;
-}
-.cont {
-  width: 1200px;
-  margin: 0 auto;
-}
-a {
-  text-decoration: none;
-  color: #000;
-}
 </style>
